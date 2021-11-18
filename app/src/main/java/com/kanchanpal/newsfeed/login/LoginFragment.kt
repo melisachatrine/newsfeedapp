19900911813765d9d9012f8185917a33c5167bdd
melisachatrine <94048613+melisachatrine@users.noreply.github.com>
2021-11-18 12:05:44 +0700

    add image to layout login plus show hide password
    add image to layout login plus show hide password

--- app/src/main/java/com/kanchanpal/newsfeed/login/LoginFragment.kt
+++ app/src/main/java/com/kanchanpal/newsfeed/login/LoginFragment.kt
@@ -1,8 +1,10 @@
 package com.kanchanpal.newsfeed.login
 
 import android.os.Bundle
+import android.text.method.SingleLineTransformationMethod
 import android.util.Patterns
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -10,14 +12,17 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.kanchanpal.newsfeed.R
 import com.kanchanpal.newsfeed.api.NewsListModel
 import com.kanchanpal.newsfeed.api.Status
 import com.kanchanpal.newsfeed.databinding.FragmentLoginNewBinding
 import com.kanchanpal.newsfeed.di.Injectable
 import com.kanchanpal.newsfeed.di.injectViewModel
+import com.kanchanpal.newsfeed.helper.AsteriskPasswordTransformationMethod
 import com.kanchanpal.newsfeed.helper.UserStorage
 import com.kanchanpal.newsfeed.helper.encrypt
 import com.kanchanpal.newsfeed.news.NewsListFragmentDirections
+import kotlinx.android.synthetic.main.fragment_login_new.*
 import javax.inject.Inject
 
 class LoginFragment : Fragment(), Injectable {
@@ -42,6 +47,31 @@ class LoginFragment : Fragment(), Injectable {
         super.onViewCreated(view, savedInstanceState)
         userStorage = UserStorage(context!!)
         viewModel = injectViewModel(viewModelFactory)
+
+        etPassword.transformationMethod = AsteriskPasswordTransformationMethod.getInstance()
+
+        initAction()
+
+    }
+
+    private fun initAction(){
+        etPassword.setOnTouchListener { _, event ->
+            val drawableRight = 2
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                if (event.rawX >= (etPassword.right - etPassword.compoundDrawables[drawableRight].bounds.width())) {
+                    if (etPassword.transformationMethod == AsteriskPasswordTransformationMethod.getInstance()) {
+                        etPassword.transformationMethod = SingleLineTransformationMethod.getInstance()
+                        etPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_visibility_eye, 0)
+                    } else {
+                        etPassword.transformationMethod = AsteriskPasswordTransformationMethod.getInstance()
+                        etPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_invisibility, 0)
+                    }
+                    return@setOnTouchListener true
+                }
+            }
+            return@setOnTouchListener false
+        }
+
         binding.btnLogin.setOnClickListener {
             if (!Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.text.toString()).matches() ) {
                 Toast.makeText(context, "Email Salah", Toast.LENGTH_LONG).show()
@@ -52,7 +82,6 @@ class LoginFragment : Fragment(), Injectable {
             }
 
         }
-
     }
 
     private fun login(userName: String, password: String) {
@@ -62,7 +91,7 @@ class LoginFragment : Fragment(), Injectable {
                     Status.SUCCESS -> {
                         binding.clProgressBar.visibility = View.GONE
                         resource.data?.let { user ->
-                            Toast.makeText(context, user.token.encrypt(), Toast.LENGTH_LONG).show()
+//                            Toast.makeText(context, user.token.encrypt(), Toast.LENGTH_LONG).show()
                             userStorage.setUser(user)
                         }
                         findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToNewsListFragment(NewsListModel()))
